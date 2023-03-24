a0a5fd855077197cca81c1a1207982203946ef64
Danilo C <>
2023-03-24 08:35:24 -0300

    Added git configs

---
+++ gitconfig.sh
@@ -0,0 +1,14 @@
+user.name=Danilo C.
+color.ui=always
+alias.co=checkout
+alias.cm=commit -m
+alias.s=status -s
+alias.sm=status -s -uno
+alias.diffc=diff --color-words
+alias.lp=log --pretty=format:'%Cred%h%Creset %C(yellow)%d%Creset %s %Cgreen(%cd)%Creset' --abbrev-commit --date=rfc
+alias.lpu=log --pretty=format:'%Cred%h%Creset %C(yellow)%d%Creset %s %Cgreen(%cr)%Creset - %C(magenta)%cn%Creset' --abbrev-commit --date=relative
+alias.lpn=log --pretty=format:'%Cred%h%Creset %C(yellow)%d%Creset %s %Cgreen(%cr)%Creset - %C(magenta)%cn%Creset' --abbrev-commit --date=relative -n
+alias.lpd=log --pretty=format:'%Cgreen%cd%Creset %Cred%h%Creset %C(yellow)%d%Creset %s' --abbrev-commit --date=short
+alias.logg=log --grep
+alias.log-tree=log --graph --oneline --decorate --all
+alias.get-alias=config --get-regexp ^alias
