cf36d2446e9e442d2121a248ee43b74f0a0f416c
Danilo C <>
2024-11-15 02:40:14 -0300

    add things at utils

--- setup/utils.sh
+++ setup/utils.sh
@@ -16,8 +16,61 @@ function get_os() {
     echo $res
 }
 
-
-
 # run(){
 #     # Run a functions and redirects the output for the adequate one
 # }
+
+
+
+###### EXAMPLE FROM GPT
+#  #!/bin/zsh
+
+#  # Function to detect the OS
+#  detect_os() {
+#      if grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null; then
+#          echo "wsl"
+#      elif [[ "$OSTYPE" == "darwin"* ]]; then
+#          echo "mac"
+#      elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+#          echo "linux"
+#      else
+#          echo "unknown"
+#      fi
+#  }
+
+#  # Function to perform WSL setup
+#  setup_wsl() {
+#      echo "Setting up for WSL..."
+#      # Add your WSL-specific setup commands here
+#  }
+
+#  # Function to perform macOS setup
+#  setup_mac() {
+#      echo "Setting up for macOS..."
+#      # Add your macOS-specific setup commands here
+#  }
+
+#  # Function to perform Linux setup
+#  setup_linux() {
+#      echo "Setting up for Linux..."
+#      # Add your Linux-specific setup commands here
+#  }
+
+#  # Main script execution
+#  os=$(detect_os)
+
+#  case $os in
+#      wsl)
+#          setup_wsl
+#          ;;
+#      mac)
+#          setup_mac
+#          ;;
+#      linux)
+#          setup_linux
+#          ;;
+#      *)
+#          echo "Unsupported OS detected."
+#          ;;
+#  esac
+######
